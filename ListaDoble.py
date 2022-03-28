806ea7354bb71df0051886ee5398cc456e08e320
EdgarAlvarez-ROL <82296299+EdgarAlvarez-ROL@users.noreply.github.com>
2022-03-27 21:15:29 -0600

    tamos bien falta estrella

--- ListaDoble.py
+++ ListaDoble.py
@@ -152,9 +152,9 @@ class Ciudad:
                 apuntador = self.root
                 while apuntador is not None:
                     # data += str(apuntador.elemento.getFilas()) + "Y"
-                    data += str(apuntador.elemento.getFila()) + "S"
-                    data += str(apuntador.elemento.getColumna()) + "S"
-                    data += str(apuntador.elemento.getCapacidad()) + "S"
+                    data += str(apuntador.elemento.getFila()) + "L"
+                    data += str(apuntador.elemento.getColumna()) + "L"
+                    data += str(apuntador.elemento.getCapacidad()) + "L"
                     apuntador = apuntador.siguiente
             return data
 
@@ -509,3 +509,243 @@ print("Prueba 9 contar elementos en la lista")
 print(nuevaListaD.contar_elementos())
 
 """
+
+
+class Robot:
+    def __init__(self, tipo, capacidad, nombre) -> None: #
+        self.tipo = tipo
+        self.capacidad = capacidad
+        self.nombre = nombre
+    
+    def getTipo(self):
+        return self.tipo
+    
+    def getCapacidad(self):
+        return self.capacidad
+
+    def getNombre(self):
+        return self.nombre
+
+
+
+class lista_Robots:
+    def __init__(self):
+        self.root = None
+
+    def insertar_lista_vacia(self, dato):
+        if self.root is None:
+            nuevoNodo = Nodo(dato)
+            self.root = nuevoNodo
+        else:
+            print("La lista no esta vacia")
+    
+    def insertar_inicio(self, dato):
+
+        if self.root is None:
+            self.insertar_lista_vacia(dato)
+        else:
+            nuevoNodo = Nodo(dato)
+            nuevoNodo.siguiente = self.root
+            self.root.anterior = nuevoNodo
+            self.root = nuevoNodo
+    
+    def insertar_final(self, dato):
+
+        if self.root is None:
+            nuevoNodo = Nodo(dato)
+            self.root = nuevoNodo
+            return
+        
+        apuntador = self.root
+
+        while apuntador.siguiente is not None:
+            apuntador = apuntador.siguiente
+
+        nuevoNodo = Nodo(dato)
+        apuntador.siguiente = nuevoNodo
+        nuevoNodo.anterior = apuntador
+    
+    def insertar_despues_elemento(self, x, dato):
+        if self.root is None:
+            print("La lista esta vacia")
+        else:
+            apuntador = self.root
+            while apuntador is not None:
+
+                if apuntador.elemento == x:
+                    break
+                apuntador = apuntador.siguiente
+            
+            if apuntador is None:
+                print("El elemento no se encuentra en la lista")
+            else:
+                nuevoNodo = Nodo(dato)
+                nuevoNodo.anterior = apuntador
+                nuevoNodo.siguiente = apuntador.siguiente
+                if apuntador.siguiente is not None:
+                    apuntador.siguiente.anterior = nuevoNodo
+                apuntador.siguiente = nuevoNodo
+    
+    def insertar_antes_elemento(self, x, dato):
+        if self.root is None:
+            print("La lista esta vacia")
+        else:
+            apuntador = self.root
+            while apuntador is not None:
+                if apuntador.elemento == x:
+                    break
+                apuntador = apuntador.siguiente
+            
+            if apuntador is None:
+                print("Elemento no se encuentra en la lista")
+            else:
+                nuevoNodo = Nodo(dato)
+                nuevoNodo.siguiente = apuntador
+                nuevoNodo.anterior = apuntador.anterior
+
+                if apuntador.anterior is not None:
+                    apuntador.anterior.siguiente = nuevoNodo
+                apuntador.anterior = nuevoNodo
+
+    def imprimir_listaD(self):
+        if self.root is None:
+            print("La lista esta vacia")
+            return
+        else:
+            apuntador = self.root
+            while apuntador is not None:
+                print(apuntador.elemento, " ")
+                apuntador = apuntador.siguiente
+
+    """"""
+    def print_Data_ROBOTS(self,numero):
+        data = ''
+        if self.root is None:
+            print("La lista esta vacia")
+            return
+        else:
+            apuntador = self.root
+            while apuntador is not None:
+                if numero == 1:
+                    print(apuntador.elemento.getTipo(), " ")
+                elif numero == 2:
+                    print(apuntador.elemento.getCapacidad(), " ")
+                elif numero == 3:
+                    print(apuntador.elemento.getNombre(), " ")
+
+                # print(apuntador.elemento, " ")
+                apuntador = apuntador.siguiente
+                # break
+        # return 
+    """"""
+    """"""
+    def return_infoEspecifica_ROBOTS(self,numero):
+        data = ''
+        if self.root is None:
+            print("La lista esta vacia")
+            return
+        else:
+            apuntador = self.root
+            while apuntador is not None:
+                if numero == 1:
+                    data += apuntador.elemento.getTipo() + " "
+                elif numero == 2:
+                    data += str(apuntador.elemento.getCapacidad()) + " "
+                elif numero == 3:
+                    data += str(apuntador.elemento.getNombre()) + " "
+            
+                # print(apuntador.elemento, " ")
+                apuntador = apuntador.siguiente
+                # break
+        return data
+    """"""
+    """"""
+    def return_Data_ROBOT(self, nombreC):
+        data = ''
+        if self.root is None:
+            print("La lista esta vacia")
+            return
+        else:
+            apuntador = self.root
+            while apuntador is not None:
+                if (apuntador.elemento.getTipo()) == nombreC:
+                    data += str(apuntador.elemento.getNombre()) + "Y"                
+                    data += str(apuntador.elemento.getCapacidad()) + "Y"
+                elif (apuntador.elemento.getNombre()) == nombreC:
+                    # data += str(apuntador.elemento.getTipo()) + "Y"                
+                    data += str(apuntador.elemento.getCapacidad())
+                # print(apuntador.elemento, " ")
+                apuntador = apuntador.siguiente
+                # break
+        return data
+        """"""
+
+    def lista_vacia(self):
+        if self.root is None:
+            return True
+        else:
+            return False
+    
+    def contar_elementos(self):
+        apuntador = self.root
+        cuenta = 0
+
+        while apuntador is not None:
+            cuenta = cuenta + 1
+            apuntador = apuntador.siguiente
+        return cuenta
+    
+    def eliminar_inicio(self):
+        if self.root is None:
+            print("La lista no contiene Nodos para eliminar")
+            return
+        
+        if self.root.siguiente is None:
+            self.root = None
+
+        self.root = self.root.siguiente
+        self.root.anterior = None
+    
+    def eliminar_final(self):
+        if self.root is None:
+            print("La lista no contiene Nodos para eliminar")
+            return
+        
+        if self.root.siguiente is None:
+            self.root = None
+            return
+
+        apuntador = self.root
+        while apuntador.siguiente is not None:
+            apuntador = apuntador.siguiente
+        apuntador.anterior.siguiente = None
+    
+    def eliminar_elemento(self, x):
+        if self.root is None:
+            print("La lista esta vacia")
+            return
+        
+        if self.root.siguiente is None:
+            if self.root.elemento == x:
+                self.root = None
+            else:
+                print("Elemento no encontrado")
+        
+        if self.root.elemento == x:
+            self.eliminar_inicio()
+            return
+        
+        apuntador = self.root
+        while apuntador.siguiente is not None:
+            if apuntador.elemento == x:
+                break
+            apuntador = apuntador.siguiente
+        
+        if apuntador.siguiente is not None:
+            apuntador.anterior.siguiente = apuntador.siguiente
+            apuntador.siguiente.anterior = apuntador.anterior
+        else:
+            if apuntador.elemento == x:
+                self.eliminar_final()
+            else:
+                return print("Elemento no encontrado")
